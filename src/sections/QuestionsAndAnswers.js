f492be157e74054739ab48d2dfb1935c4f418d16
imri <imri.paran@mail.huji.ac.il>
2019-12-24 17:19:24 +0200

    bold quiestion

--- src/sections/QuestionsAndAnswers.js
+++ src/sections/QuestionsAndAnswers.js
@@ -5,25 +5,28 @@ import SectionTitle from "../SectionTitle";
 import Section from "../components/Section";
 import MyDivider from "../components/MyDivider";
 
+const Question = ({ children }) => (
+  <Typography variant="subtitle1" style={{fontWeight: 550}}>{children}</Typography>
+);
+const Answer = ({ children }) => (
+  <Typography variant="subtitle1">{children}</Typography>
+);
+
 export default ({ even }) => (
   <Section even={even}>
     <MainContainer even={even}>
       <SectionTitle>שאלות ותשובות</SectionTitle>
       <MyDivider />
-      <Typography variant="subtitle1">מי מזמן אותי לגיבוש?</Typography>
-      <Typography variant="subtitle2">מיטב</Typography>
-      <Typography variant="subtitle1">
-        אם לא הייתי בגדנע האם אני יכול להגיע לגיבוש?
-      </Typography>
-      <Typography variant="subtitle2">
-        לא, רק מי שהיה בגדנע יכול להגיע לגיבוש עוקץ
-      </Typography>
-      <Typography variant="subtitle1">
+      <Question>מי מזמן אותי לגיבוש?</Question>
+      <Answer variant="subtitle2">מיטב</Answer>
+      <Question>אם לא הייתי בגדנע האם אני יכול להגיע לגיבוש?</Question>
+      <Answer>לא, רק מי שהיה בגדנע יכול להגיע לגיבוש עוקץ</Answer>
+      <Question>
         אם הייתי בגדנע הקודמת ועשיתי מכינה ואני רוצה לעשות גיבוש אני יכול?
-      </Typography>
-      <Typography variant="subtitle2">
+      </Question>
+      <Answer>
         היית צריך לעשות את הגיבוש בשנה שעברה, מקרים פרטניים יעברו לבחינה של מיטב
-      </Typography>
+      </Answer>
     </MainContainer>
   </Section>
 );
